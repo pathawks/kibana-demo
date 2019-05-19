3011d58e8358d3125b4a51b36d24fde49a2f8776
Pat Hawks <pat@pathawks.com>
2019-05-19 17:02:18 -0500

    more orders!

--- generate.rb
+++ generate.rb
@@ -1,7 +1,7 @@
 require_relative "order"
 require_relative "random"
 
-for i in 1..3500 do
+for i in 1..35000 do
 	startDate = Time.parse('2019-05-13T18:30:00.000-0500') + randomTimeOffset
 	order(startDate)
 end
